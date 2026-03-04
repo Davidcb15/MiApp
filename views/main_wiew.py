8a72cb20257823380e82ef761b42faac81c3bb75
David CB <davidcb230718@gmail.com>
2026-03-04 16:58:22 +0100

    deleting files

--- views/main_wiew.py
+++
@@ -1,9 +0,0 @@
-# main_view.py
-
-from PyQt6 import uic
-from PyQt6.QtWidgets import QMainWindow
-
-class MainView(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("mainwindow.ui", self)
